f2398c26e1ba931818121bec0b3246cc0b1a2a15
Kourtney McCullough <kourtneyjmccullough@gmail.com>
2019-10-27 14:28:30 -0500

    added styling to FriendCard

--- src/components/FriendCard/index.js
+++ src/components/FriendCard/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./style.css";
+
 const FriendCard = props => (
   <div onClick={() => props.setClicked(props.id)} className="card col-md-3">
     <div className="img-container">
