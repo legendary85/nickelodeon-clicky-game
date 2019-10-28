14d0b98627ddbe8d52b751b8bfa34900704bfebd
Kourtney McCullough <kourtneyjmccullough@gmail.com>
2019-10-27 19:28:06 -0500

    media query to make app mobile responsive

--- src/components/FriendCard/index.js
+++ src/components/FriendCard/index.js
@@ -2,9 +2,12 @@ import React from "react";
 import "./style.css";
 
 const FriendCard = props => (
-  <div onClick={() => props.setClicked(props.id)} className="card col-md-3">
+  <div
+    onClick={() => props.setClicked(props.id)}
+    className="card col-md-3 col-3"
+  >
     <div className="img-container">
-      <img alt={props.image} src={props.image} />
+      <img className="img" alt={props.image} src={props.image} />
     </div>
   </div>
 );
